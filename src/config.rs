9bac90c8835ea16e04e939aa17a86f954b9626b4
Filip Hornak <sleepy771@gmail.com>
2018-03-12 02:49:04 +0100

    Create settings file

--- src/config.rs
+++ src/config.rs
@@ -1,29 +1,108 @@
+use std::path::Path;
+use std::convert::AsRef;
+use std::ffi::OsStr;
+use std::fs::File;
+use serde_yaml;
 
-const THRITY_SECONDS: u32 = 30 * 60 * 1000;
+const DEFAUL_PATH: &'static str = ".ip_aggregator/settings.yaml";
 
 
+#[derive(PartialEq,Eq,Serialize,Deserialize,Debug,Clone)]
+pub struct UdpSettings {
+    #[serde(default = "default_udp_receiver")]
+    receiver_address: Option<String>,
+    #[serde(default = "default_udp_sender")]
+    send_to: Option<String>,
+}
+
+#[derive(Deserialize,PartialEq,Eq,Debug,Clone)]
 pub struct Settings {
-    receiver_address: String,
-    publish_address: String,
-    use_udp: bool,
+    udp: Option<UdpSettings>,
+    #[serde(default = "thrity_seconds")]
     publish_timer: u32,
+
+    #[serde(default = "default_add_broadcast")]
     auto_add_broadcast: bool,
+    #[serde(default = "default_add_zeroed")]
     auto_add_zeroed: bool,
 }
 
 impl Settings {
     pub fn defualt() -> Settings {
         Settings {
-            receiver_address: "127.0.0.1:6788".to_string(),
-            publish_address: "127.0.0.1:6789".to_string(),
-            use_udp: true,
-            publish_timer: THRITY_SECONDS,
-            auto_add_zeroed: true,
-            auto_add_broadcast: true
+            udp: Some(UdpSettings {
+                receiver_address: default_udp_receiver(),
+                send_to: default_udp_sender()
+            }),
+            publish_timer: thrity_seconds(),
+            auto_add_zeroed: default_add_zeroed(),
+            auto_add_broadcast: default_add_broadcast()
+        }
+    }
+
+    pub fn is_using_udp(&self) -> bool {
+        match self.udp {
+            Some(_) => true,
+            None => false
+        }
+    }
+
+    pub fn is_last_node_with_settings(&self) -> bool {
+        self.auto_add_broadcast || self.auto_add_zeroed
+    }
+
+    pub fn add_zeroed(&self) -> bool {
+        self.auto_add_zeroed
+    }
+
+    pub fn add_boradcast(&self) -> bool {
+        self.auto_add_broadcast
+    }
+}
+
+pub fn load_from_default_location(root: &Path) -> Result<Settings, String> {
+    load_from_file(&root.join(&Path::new(DEFAUL_PATH)))
+}
+
+pub fn load_from_file(path: &Path) -> Result<Settings, String> {
+    println!("Attempted load of settings from `{}`", path.display());
+    if ! path.exists() {
+        return Err(format!("File `{}` does not exist!", path.display()));
+    }
+    
+    match File::open(&path) {
+        Err(reason) => {
+            Err(format!("File `{}` can not be open: {}", path.display(), reason))
+        },
+        Ok(file) => {
+            match serde_yaml::from_reader(file) {
+                Err(reason) => Err(format!("Parsing settings file `{}` failed: {}", path.display(), reason)),
+                Ok(settings) => Ok(settings)
+            }
         }
     }
 }
 
-pub fn load_from_file(filepath: String) -> Settings {
-    Settings::defualt()
+pub fn settings_path_exists(root: &Path) -> bool {
+    root.join(&Path::new(DEFAUL_PATH)).exists()
+}
+
+fn default_add_broadcast() -> bool {
+    true
+}
+
+fn default_add_zeroed() -> bool {
+    true
+}
+
+fn default_udp_receiver() -> Option<String> {
+    Some("127.0.0.1:6788".to_string())
+}
+
+fn default_udp_sender() -> Option<String> {
+    Some("127.0.0.1:6789".to_string())
+}
+
+fn thrity_seconds() -> u32 {
+    30 * 60 * 1000
 }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,56 @@
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_yaml;
+
+#[macro_use]
+extern crate lazy_static;
+
 mod subnet_tree;
 mod udp_listener;
 mod config;
 
 use subnet_tree::{IPTree, OctetNode};
+use config::{Settings, load_from_file, load_from_default_location};
+use std::env::home_dir;
+use std::path::PathBuf;
+
+
+lazy_static! {
+    pub static ref SETTINGS: Settings = {
+        let paths = {
+            let mut paths = Vec::new();
+            if let Some(home_path) = home_dir() {
+                paths.push(home_path.clone());
+            }
+            paths.push(PathBuf::from("/etc".to_string()));
+            paths
+        };
+        for path in paths {
+            match load_from_default_location(&path) {
+                Ok(settings) => return settings,
+                Err(reason) => println!("Can not load file: {}", reason)
+            }
+        }
+        Settings::defualt()
+    };
+}
 
 fn main() {
-    let mut tree = IPTree::new();
-    tree.add(&[2, 9, 18, 22]);
-    tree.add(&[2, 9, 18, 21]);
-    tree.add(&[2, 9, 18, 20]);
-    tree.add(&[127, 0, 0, 1]);
-    for k in 0 .. 255 {
-        for j in 0 .. 255 {
-            for i in 0 .. 255 {
-                tree.add(&[172, k, j, i]);
-            }
-            tree.add(&[172, k, j, 255]);
-        }
-        tree.add(&[172, k, 255, 255]);
-    }
-    println!("{:?}", tree.list_cidr());
+//    let mut tree = IPTree::new();
+//    tree.add(&[2, 9, 18, 22]);
+//    tree.add(&[2, 9, 18, 21]);
+//    tree.add(&[2, 9, 18, 20]);
+//    tree.add(&[127, 0, 0, 1]);
+//    for k in 0 .. 255 {
+//        for j in 0 .. 255 {
+//            for i in 0 .. 255 {
+//                tree.add(&[172, k, j, i]);
+//            }
+//        }
+//        tree.add(&[172, k, 255, 255]);
+//    }
+//    println!("{:?}", tree.list_cidr());
+    let settings = load_from_file(&PathBuf::from("settings_test.conf.yaml")).unwrap();
+    println!("{:?}", settings);
+    println!("{:?}", SETTINGS.add_zeroed());
 }
