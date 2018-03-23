5a72a6a75ccb9fa606ecce08dda136c7831b8e20
Filip Hornak <sleepy771@gmail.com>
2018-03-23 18:39:44 +0100

    Create integration test for UdpSender::run_sender.

--- src/udp.rs
+++ src/udp.rs
@@ -78,6 +78,9 @@ impl UdpSender {
         loop {
             match self.receiver.recv() {
                 Ok(ip_vec) => {
+                    if ip_vec == vec!["STOP!".to_string()] {
+                        break;
+                    }
                     for ip_string in (self.formatter)(ip_vec) {
                         self.socket.send_to(ip_string.as_bytes(), self.send_to.as_str()).unwrap();
                     }
@@ -200,5 +203,59 @@ mod tests {
         }
         assert_eq!(vec![[192, 168, 1, 1], [127, 0, 0, 1], [172, 16, 100, 10]], data);
 
+    }
+
+    #[test]
+    fn test_UdpSender_run_sender() {
+        use std::thread;
+        use std::sync::mpsc::channel;
+        use std::string::ToString;
+
+
+
+        let data = vec![
+            "192.168.2.1/32".to_string(),
+            "172.16.100.1/24".to_string(),
+            "10.10.1.1/16".to_string()];
+        
+        let mut handles = Vec::new();
+
+        let (mut udp_listener_tx, mut udp_listener_rx) = channel();
+        handles.push(thread::spawn(move || {
+            let mut socket = UdpSocket::bind("127.0.0.1:13345").unwrap();
+            
+            let mut buffer: [u8; 2048] = [0; 2048];
+
+            match socket.recv_from(&mut buffer) {
+                Ok((len, addr)) => {
+                    udp_listener_tx.send(str::from_utf8(&buffer[0 .. len]).unwrap().to_string()).unwrap();
+                }
+
+                Err(e) => panic!("Error occured during receive udp datagram: {}", e)
+            }
+        }));
+
+        thread::sleep_ms(3000);
+
+        let (mut tx, mut rx) = channel();
+
+        handles.push(thread::spawn(move || {
+            let sender = UdpSender::new("127.0.0.1:13345", simpl_formatter, rx);
+            sender.run_sender();
+        }));
+
+        tx.send(data).unwrap();
+        tx.send(vec!["STOP!".to_string()]);
+        thread::sleep_ms(5000);
+        drop(tx);
+
+        let recv_data = udp_listener_rx.recv().unwrap();
+        drop(udp_listener_rx);
+
+        assert_eq!("192.168.2.1/32 172.16.100.1/24 10.10.1.1/16".to_string(), recv_data);
+
+        for handle in handles {
+            handle.join().unwrap();
+        }
     }
 }
