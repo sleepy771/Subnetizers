97549645f90c895f62b219697ccfd7604d9b5dee
Filip Hornak <sleepy771@gmail.com>
2018-03-26 05:43:41 +0200

    Create command line arguments

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ mod parsers;
 mod listeners;
 mod formatters;
 
-use argparse::{ArgumentParser, StoreOption, StoreTrue};
+use argparse::{ArgumentParser, StoreOption, StoreTrue, Store, Collect};
 use config::{load_from_default_location, load_from_file, Settings};
 use ipagg::IpAggregator;
 use std::env::home_dir;
@@ -50,15 +50,25 @@ lazy_static! {
 
 fn read_cmd_line_args() -> (Option<String>, bool) {
     let mut settings_path: Option<String> = None;
-    let mut start_udp: bool = false;
+    let mut receiver: String = "udp".to_owned();
+    let mut kafka_hosts: Vec<String> =  Vec::new();
+    let mut kafka_topic: Option<String> = None;
+    let mut kafka_group: Option<String> = None;
+    let mut udp_recv_host: Option<String> = None;
+    let mut udp_send_host: Option<String> = None;
     {
         let mut ap: ArgumentParser = ArgumentParser::new();
         ap.set_description("Small uService for IPv4 Addresses aggregation in standard CIDR format.");
         ap.refer(&mut settings_path).add_option(&["-c", "--config-path"], StoreOption, "Alternative config file path.");
-        ap.refer(&mut start_udp).add_option(&["-u", "--udp"], StoreTrue, "Start as UDP server");
+        ap.refer(&mut receiver).add_option(&["-r", "--receiver"], Store, "Receiver type. Defaults to `udp`. Possible options are [`udp`, `kafka`].");
+        ap.refer(&mut kafka_hosts).add_option(&["--kafka-hosts"], Collect, "Kafka hosts, if kafka option is specified.");
+        ap.refer(&mut kafka_topic).add_option(&["--kafka-receiver-topic"], StoreOption, "Kafka consumer topic.");
+        ap.refer(&mut kafka_group).add_option(&["--kafka-receiver-group"], StoreOption, "Kafka group.");
+        ap.refer(&mut udp_recv_host).add_option(&["--udp-receiver-host"], StoreOption, "Udp receiver host.");
+        ap.refer(&mut udp_send_host).add_option(&["--udp-send-to"], StoreOption, "Udp send to host.");
         ap.parse_args_or_exit();
     };
-    (settings_path, start_udp)
+    (settings_path, true)
 }
 
 fn main() {
