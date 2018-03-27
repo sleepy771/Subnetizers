b03bfcf6137d47dc082cc5d9fee78c13a6e18810
Filip Hornak <sleepy771@gmail.com>
2018-03-27 17:44:18 +0200

    uncomment integration udp tests

--- src/senders.rs
+++ src/senders.rs
@@ -92,61 +92,61 @@ pub mod kafka {
     }
 }
 
-//#[cfg(test)]
-//mod tests {
-//    use super::*;
-//    use std::str;
-//
-//    #[test]
-//    fn test_UdpSender_run_sender() {
-//        use std::thread;
-//        use std::sync::mpsc::channel;
-//        use std::string::ToString;
-//        use formatters::simple_formatter;
-//
-//        let data = vec![
-//            "192.168.2.1/32".to_string(),
-//            "172.16.100.1/24".to_string(),
-//            "10.10.1.1/16".to_string()];
-//
-//        let mut handles = Vec::new();
-//
-//        let (mut udp_listener_tx, mut udp_listener_rx) = channel();
-//        handles.push(thread::spawn(move || {
-//            let mut socket = UdpSocket::bind("127.0.0.1:13345").unwrap();
-//
-//            let mut buffer: [u8; 2048] = [0; 2048];
-//
-//            match socket.recv_from(&mut buffer) {
-//                Ok((len, addr)) => {
-//                    udp_listener_tx.send(str::from_utf8(&buffer[0..len]).unwrap().to_string()).unwrap();
-//                }
-//
-//                Err(e) => panic!("Error occured during receive udp datagram: {}", e)
-//            }
-//        }));
-//
-//        thread::sleep_ms(3000);
-//
-//        let (mut tx, mut rx) = channel();
-//
-//        handles.push(thread::spawn(move || {
-//            let sender = UdpSender::new("127.0.0.1:13345", simple_formatter, rx);
-//            sender.run_sender();
-//        }));
-//
-//        tx.send(data).unwrap();
-//        tx.send(vec!["STOP!".to_string()]);
-//        thread::sleep_ms(5000);
-//        drop(tx);
-//
-//        let recv_data = udp_listener_rx.recv().unwrap();
-//        drop(udp_listener_rx);
-//
-//        assert_eq!("192.168.2.1/32 172.16.100.1/24 10.10.1.1/16".to_string(), recv_data);
-//
-//        for handle in handles {
-//            handle.join().unwrap();
-//        }
-//    }
-//}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::str;
+
+    #[test]
+    fn test_UdpSender_run_sender() {
+        use std::thread;
+        use std::sync::mpsc::channel;
+        use std::string::ToString;
+        use formatters::simple_formatter;
+
+        let data = vec![
+            "192.168.2.1/32".to_string(),
+            "172.16.100.1/24".to_string(),
+            "10.10.1.1/16".to_string()];
+
+        let mut handles = Vec::new();
+
+        let (mut udp_listener_tx, mut udp_listener_rx) = channel();
+        handles.push(thread::spawn(move || {
+            let mut socket = UdpSocket::bind("127.0.0.1:13345").unwrap();
+
+            let mut buffer: [u8; 2048] = [0; 2048];
+
+            match socket.recv_from(&mut buffer) {
+                Ok((len, addr)) => {
+                    udp_listener_tx.send(str::from_utf8(&buffer[0..len]).unwrap().to_string()).unwrap();
+                }
+
+                Err(e) => panic!("Error occured during receive udp datagram: {}", e)
+            }
+        }));
+
+        thread::sleep_ms(3000);
+
+        let (mut tx, mut rx) = channel();
+
+        handles.push(thread::spawn(move || {
+            let sender = UdpSender::new("127.0.0.1:13345", simple_formatter, rx);
+            sender.run_sender();
+        }));
+
+        tx.send(data).unwrap();
+        tx.send(vec!["STOP!".to_string()]);
+        thread::sleep_ms(5000);
+        drop(tx);
+
+        let recv_data = udp_listener_rx.recv().unwrap();
+        drop(udp_listener_rx);
+
+        assert_eq!("192.168.2.1/32 172.16.100.1/24 10.10.1.1/16".to_string(), recv_data);
+
+        for handle in handles {
+            handle.join().unwrap();
+        }
+    }
+}
